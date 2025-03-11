e913cd5b9e4d21d7ff8c97b15e4f4538fccf3fb9
pedrolkaas <151377553+pedrolkaas@users.noreply.github.com>
2025-03-11 17:58:14 +0000

    FINALIZADO

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,25 @@
+int main() {
+int idade;
+int matricula;
+float altura;
+char nome[50];
 
+printf("digite sua idade: \n");
+scanf("%d", &idade);
+
+printf("digite sua altura: \n");
+scanf("%f", &altura);
+
+printf("digite seu nome: \n");
+scanf("%s", &nome);
+
+printf("digite sua matricula: \n");
+scanf("%d", &matricula);
+
+printf("nome do aluno: %s - matricula: %d\n", nome, matricula);
+printf("altura: %f - idade: %d", altura, idade);
+
+
+return 0;
+
+}
